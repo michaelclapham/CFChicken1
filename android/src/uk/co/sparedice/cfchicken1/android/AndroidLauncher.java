e21e47448e8000b2672e7cd25c02456830d17d52
Michael Clapham <michaeljclapham@gmail.com>
2014-11-08 00:00:43 +0000

    added libgdx generated project

---
+++ android/src/uk/co/sparedice/cfchicken1/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package uk.co.sparedice.cfchicken1.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import uk.co.sparedice.cfchicken1.CFChicken1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CFChicken1(), config);
+	}
+}
---
+++ desktop/src/uk/co/sparedice/cfchicken1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.sparedice.cfchicken1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.sparedice.cfchicken1.CFChicken1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CFChicken1(), config);
+	}
+}
